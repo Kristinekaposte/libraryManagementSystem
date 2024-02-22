7605277b90e778388b6985f5a2efdaa109adb8da
kristine.kaposte <kristine.kaposte@accenture.com>
2024-02-22 13:06:37 +0200

    1_Add_Book_and_BookDAO_classes_for_library_management

---
+++ src/main/java/com/librarymanagementsystem/business/repository/model/BookDAO.java
@@ -0,0 +1,25 @@
+package com.librarymanagementsystem.business.repository.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "book")
+public class BookDAO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "author", nullable = false, length = 30, unique = true)
+    private String author;
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;
+    @Column(name = "price", nullable = false)
+    private String price;
+    @Column(name = "description")
+    private String description;
+}
---
+++ src/main/java/com/librarymanagementsystem/model/Book.java
@@ -0,0 +1,19 @@
+package com.librarymanagementsystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class Book {
+
+    private Long id;
+    private String author;
+    private String title;
+    private String price;
+    private String description;
+}
