ce1f5b145331d803b4b426ca259c4d0faa66181c
kristine.kaposte <kristine.kaposte@accenture.com>
2024-03-07 14:12:05 +0200

    4_Added_OpenAPI_configuration

---
+++ src/main/java/com/librarymanagementsystem/openAPI/DescriptionVariables.java
@@ -0,0 +1,5 @@
+package com.librarymanagementsystem.openAPI;
+
+public class DescriptionVariables {
+    public static final String BOOK = "Book Controller";
+}
---
+++ src/main/java/com/librarymanagementsystem/openAPI/OpenAPIConfig.java
@@ -0,0 +1,32 @@
+package com.librarymanagementsystem.openAPI;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Library Management System API",
+                description = "Library Management System API",
+                version = "1.0"
+        ))
+public class OpenAPIConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-api")
+                .pathsToMatch("api/v1/library/management/system/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi privateApi() {
+        return GroupedOpenApi.builder()
+                .group("private-api")
+                .packagesToScan("com.librarymanagementsystem.web.controller")
+                .build();
+    }
+}
